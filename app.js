0b113100961650a52f2983d88028f42711e03ddd
Matthew Quint <mattquint111@gmail.com>
2020-12-11 13:14:26 -0600

    added Procfile

--- app.js
+++ app.js
@@ -8,30 +8,36 @@ app.use(express.json())
 
 const PORT = 8080
 
-// test score //
-//build score model
-// let testScore = models.SnakeScore.build({
-//     name: "Jane Doe",
-//     score: 9001,
-//     difficulty: 'hard'
-// })
-// // send score model to DB
-// testScore.save()
-//     .then( (persistedScore) => {
-//         console.log(persistedScore)
-//     })
-// // read all     scores from DB
-// models.SnakeScore.findAll()
-//     .then( scores => console.log(scores))
-// find score by PK
-models.SnakeScore.findAll({
-    where: {
-        id: 2
-    }
-}).then( score => console.log(score))
-
 app.get('/test', (req,res) => {
     res.send('Server is working!')
+})
+
+app.get('/snake-scores', async (req, res) => {
+    let snakeScores = await models.SnakeScore.findAll({
+        order: [
+            ["score", "DESC"]
+        ],
+        limit: 10
+    })
+
+    res.send(snakeScores)
+})
+
+app.post('/snake-scores', async (req,res) => {
+    const name = req.body.name
+    const score = req.body.score
+    const difficulty = req.body.difficulty
+
+    let snakeScore = await models.SnakeScore.create({
+        name: name,
+        score: score,
+        difficulty: difficulty
+    })
+
+    console.log(snakeScore)
+
+    res.send(snakeScore)
+
 })
 
 app.listen(PORT, () => {
